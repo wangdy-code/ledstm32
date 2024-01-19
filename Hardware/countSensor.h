0078a93105b179d9587203d9001ddc16a6caa973
delta <1158046762@qq.com>
2024-01-19 22:10:39 +0800

    合并功能

---
+++ Hardware/countSensor.h
@@ -0,0 +1,4 @@
+#ifndef __COUNTSENSOR_H
+void CountSensor_Init();
+uint16_t CountSensor_GetCounts();
+#endif // !__COUNTSENSOR_H
---
+++ Hardware/encoder.h
@@ -0,0 +1,4 @@
+#ifndef __ENCODER_H
+void Encoder_Init();
+int16_t Encoder_Get();
+#endif // !__ENCODER_H
