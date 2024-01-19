9e550f3df4cf508fb46743cf2b024453c08a6ecc
delta <1158046762@qq.com>
2024-01-19 09:15:47 +0800

    暂存 外部中断

---
+++ Hardware/countSensor.h
@@ -0,0 +1,3 @@
+#ifndef __COUNTSENSOR_H
+void CountSensor_Init();
+#endif // !__COUNTSENSOR_H
--- src/main.c
+++ src/main.c
@@ -10,33 +10,15 @@
  */
 
 #include "stm32f10x.h"
-#include "led.h"
-#include "delay.h"
-#include "key.h"
-#include "buzzer.h"
-#include "lightSensor.h"
 
-#define LED_PERIPH RCC_APB2Periph_GPIOA
-#define LED_PORT   GPIOA
-#define LED_PIN    GPIO_Pin_0
+#include "countSensor.h"
 
-#define KEY_PIN    GPIO_Pin_1
+
 
 int main()
 {
-    Delay_Init();
-    KEY_Init(GPIOB, KEY_PIN, GPIO_Mode_IPU, GPIO_Speed_50MHz);
-    LED_Init(GPIOA, LED_PIN, GPIO_Mode_Out_PP, GPIO_Speed_50MHz);
-    Buzzer_Init(GPIOB, GPIO_Pin_12, GPIO_Mode_Out_PP, GPIO_Speed_50MHz);
-    LightSensor_Init(GPIOB, GPIO_Pin_13, GPIO_Mode_IPU, GPIO_Speed_50MHz);
 
     while (1) {
-        if (LightSensor_Get(GPIOB, GPIO_Pin_13) == 1) {
-            Buzzer_ON(GPIOB, GPIO_Pin_12);
-            LED_ON(GPIOA,GPIO_Pin_0);
-        } else {
-            Buzzer_OFF(GPIOB, GPIO_Pin_12);
-            LED_OFF(GPIOA,GPIO_Pin_0);
-        }
+        
     }
 }
