917cd9999e3dea74cc049650dc4762365cfc662a
delta <1158046762@qq.com>
2024-01-19 16:25:39 +0800

    旋转编码器

--- src/main.c
+++ src/main.c
@@ -11,18 +11,18 @@
 
 #include "stm32f10x.h"
 
-#include "countSensor.h"
 #include "OLED.h"
+#include "encoder.h"
 
-
+int16_t Num = 0;
 int main()
 {
     OLED_Init();
-    CountSensor_Init();
-    OLED_ShowString(1,1,"CountSensor");
+    Encoder_Init();
+    OLED_ShowString(1, 1, "encoder");
     while (1) {
-        OLED_ShowString(2,1,"counts: ");
-        OLED_ShowNum(2,10,CountSensor_GetCounts(),3);
-        
+        Num += Encoder_Get();
+        OLED_ShowString(2, 1, "Num: ");
+        OLED_ShowSignedNum(2, 8, Num, 5);
     }
 }
